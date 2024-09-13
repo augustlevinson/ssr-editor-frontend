0f70406e12753477e337e0629a88e664f0ef1ebc
caas23 <caas23@student.bth.se>
2024-09-13 09:53:27 +0200

    refactored document list

--- src/views/Documents.js
+++ src/views/Documents.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import DocumentList from "../components/DocumentList";
 
 function Documents() {
-    const [documents, setDocuments] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:1337/")
-            .then((response) => response.json())
-            .then((data) => setDocuments(data.docs))
-            .catch((error) => console.error("Error fetching documents:", error));
-    }, []);
-
     return (
-        <div>
-            <h1>Documents</h1>
-                {documents.map((doc) => (
-                    <h2 key={doc.id}>{doc.title}</h2>
-                ))}
+        <div className="main">
+            <h1>Dokument</h1>
+            < DocumentList />
         </div>
     );
 }
