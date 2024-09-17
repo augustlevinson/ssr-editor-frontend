a377ed0d142d33a58deeabe9ca767b69d2520413
August Levinson <118256272+920523@users.noreply.github.com>
2024-09-17 13:54:49 +0200

    added failing test to check CI chain

--- src/test/Elements.test.js
+++ src/test/Elements.test.js
@@ -8,8 +8,8 @@ test('renders database reset link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-// test('Component should have an <h2>', async () => {
-//   const response = render(<Documents />);
-//   const heading = screen.getByRole('heading', { level: 1 });
-//   expect(heading).toBeInTheDocument();
-// });
+test('Component should have an <h2>', async () => {
+  const response = render(<Documents />);
+  const heading = screen.getByRole('heading', { level: 1 });
+  expect(heading).toBeInTheDocument();
+});
