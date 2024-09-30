ce9aed15b64920a852fbe15fa4077dd06f03313d
caas23 <caas23@student.bth.se>
2024-09-30 12:29:25 +0200

    Added temporary footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@ function Footer() {
     <footer>
       <p className="copyright">&copy; Calle Andersson & August Levinson</p>
       <a href={`${baseUrl}/reset`}>Återställ databas</a>
+      {/* Länkar för att underlätta vid utveckling, ta bort senare */}
+      <a style={{marginLeft: 1 + 'em'}} href={`${baseUrl}/signup`}>Skapa användare</a>
+      <a style={{marginLeft: 1 + 'em'}} href={`${baseUrl}/login`}>Logga in</a>
     </footer>
   );
 }
