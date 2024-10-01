e2e82fca5a492a8f9213677d4e90156e858d0f83
August Levinson <118256272+920523@users.noreply.github.com>
2024-10-01 13:40:48 +0200

    WIP cookie token

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ function Footer() {
     <footer>
       <p className="copyright">&copy; Calle Andersson & August Levinson</p>
       <a href={`${baseUrl}/reset`}>Återställ databas</a>
+      <a style={{marginLeft: 1 + 'em'}} href={`${baseUrl}/signup`}>Skapa användare</a>
+      <a style={{marginLeft: 1 + 'em'}} href={`${baseUrl}/login`}>Logga in</a>
     </footer>
   );
 }
