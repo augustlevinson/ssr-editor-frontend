ca1d5ea844d5ec50f94d43f7f2f28a2b4be538bd
caas23 <caas23@student.bth.se>
2024-10-25 11:41:34 +0200

    Removed create user link from logged in mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
     <footer>
       <p className="copyright">&copy; Calle Andersson & August Levinson</p>
       <a href={`${baseUrl}/reset`}>Återställ databas</a>
-      <a href={`${baseUrl}/signup`}>Skapa användare</a>
+      {!props.user && <a href={`${baseUrl}/signup`}>Skapa användare</a>}
       {!props.user && <a href={`${baseUrl}/login` }>Logga in</a>}
       {props.user && <a href={`${baseUrl}/logout`}>Logga ut</a>}
     </footer>
